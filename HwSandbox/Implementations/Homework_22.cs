8899c856b8606c7ff7d0c4b63d23b39ff4629a22
lil-drow <lil_drow@outlook.com>
2026-01-29 02:12:52 +0500

    Реализована основа игры в "Угадай число"
    ToDo: привести реализацию в соответствие принципам SOLID

--- HwSandbox/Implementations/Homework_22.cs
+++ HwSandbox/Implementations/Homework_22.cs
@@ -8,7 +8,9 @@ namespace HwSandbox.Implementations;
    
    Описание/Пошаговая инструкция выполнения домашнего задания:
    На примере реализации игры «Угадай число» продемонстрировать практическое применение SOLID принципов.
-   Программа рандомно генерирует число, пользователь должен угадать это число. При каждом вводе числа программа пишет больше или меньше отгадываемого. Кол-во попыток отгадывания и диапазон чисел должен задаваться из настроек.
+   Программа рандомно генерирует число, пользователь должен угадать это число. 
+   При каждом вводе числа программа пишет больше или меньше отгадываемого. 
+   Кол-во попыток отгадывания и диапазон чисел должен задаваться из настроек.
    В отчёте написать, что именно сделано по каждому принципу.
    Приложить ссылку на проект и написать, сколько времени ушло на выполнение задачи.
    
@@ -30,6 +32,146 @@ public class Homework_22: Homework
     }
     public override void Go()
     {
-        
+        Console.WriteLine("Лекция 22. Принципы SOLID:");
+        Console.WriteLine("------------------");
+        IPlayable Game = new GuessNumGame(new GuessNumGameConsoleConfiguration());
+        bool letUsPlay = true;
+        Game.Play();
+        while (letUsPlay)
+        {
+            Console.WriteLine("\nСыграем снова?\nВведите yes (y) или да (д), чтобы продолжить)");
+            string answer = Console.ReadLine().ToLower();
+            if (answer is "y" or "д" or "yes" or "да")
+            {
+                Game.Play();
+            }
+            else
+            {
+                letUsPlay = false;
+                Console.WriteLine("\nКонец игры.");
+            }
+        }
+        Console.WriteLine("------------------");
+    }
+    
+}
+
+public interface IPlayable
+{
+    void Play();
+}
+
+public interface IConfigurable
+{
+    public int MinValue { get; } 
+    public int MaxValue { get; }
+    public int AttemptsNum { get; }
+    void Configure();
+}
+
+public class GuessNumGameConsoleConfiguration : IConfigurable
+{
+    public int MinValue { get; private set; } 
+    public int MaxValue { get; private set; }
+    public int AttemptsNum { get; private set; }
+
+    public void Configure()
+    {
+        Console.WriteLine("Для начала настроим игру.");
+        Console.WriteLine("Определим количество попыток на отгадывание числа.");
+        AttemptsNum = Extensions.GetPositiveNumValue();
+        Console.WriteLine("\nОпределим нижнюю границу диапазона.");
+        MinValue = Extensions.GetNumValue();
+        Console.WriteLine("\nОпределим верхнюю границу диапазона.");
+        while (MinValue >= MaxValue)
+        {
+            Console.WriteLine("\nЗначение верхней границы должно быть больше значения нижней.");
+            MaxValue = Extensions.GetNumValue();
+        }
     }
 }
+
+public class GuessNumGame : IPlayable
+{
+    IConfigurable _config;
+    int _refNum;
+    int _attemptsNum;
+    public GuessNumGame(IConfigurable config)
+    {
+        _config = config;
+        _config.Configure();
+    }
+    public void Play()
+    {
+        _attemptsNum = _config.AttemptsNum;
+        Console.WriteLine("\nЗагадываю число...");
+        _refNum = Extensions.GenerateRefNum(_config.MinValue, _config.MaxValue);
+        Console.WriteLine("Готово!");
+        bool userWon = false;
+        while (!userWon && _attemptsNum > 0)
+        {
+            _attemptsNum--;
+            Console.WriteLine("\nКакое число было загадано?");
+            int guessedNum = Extensions.GetNumValue();
+            userWon = Extensions.CompareNums(_refNum, guessedNum);
+        }
+        if (userWon)
+        {
+            Console.WriteLine($"Вы выиграли!\nБыло загадано число {_refNum}.");
+        }
+        else
+        {
+            Console.WriteLine("Число попыток исчерпано!");
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static int GetNumValue()
+    {
+        string input = string.Empty;
+        int result = 0;
+        while (!int.TryParse(input, out result))
+        {
+            Console.WriteLine("Введите целочисленное значение.");
+            input = Console.ReadLine();
+        }
+        return result;
+    } 
+    public static int GetPositiveNumValue()
+    {
+        string input = string.Empty;
+        int result = 0;
+        while (!int.TryParse(input, out result) || result <= 0)
+        {
+            Console.WriteLine("Введите целочисленное значение больше нуля.");
+            input = Console.ReadLine();
+        }
+        return result;
+    } 
+    // загадываем число
+    public static int GenerateRefNum(int min, int max)
+    {
+        Random rnd = new Random();
+        return rnd.Next(min, max);
+    }
+    // обрабатываем предположение пользователя: сравниваем предполагаемое число с загаданным
+    public static bool CompareNums(int refNum, int guessedNum)
+    {
+        if (guessedNum == refNum)
+        {
+            return true;
+        }
+        if (guessedNum < refNum)
+        {
+            Console.WriteLine("\nВаше число меньше загаданного.");
+        }
+        else
+        {
+            Console.WriteLine("\nВаше число больше загаданного.");
+        }
+        return false;
+    }
+}
+
